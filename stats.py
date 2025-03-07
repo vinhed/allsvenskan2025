ae8f881b23d055f1684a813ef28085ca6013822f
vinhed <vinhed@gmail.com>
2025-03-07 23:05:23 +0100

    modifed code

--- stats.py
+++ stats.py
@@ -15,17 +15,26 @@ with open('README.md', 'r', encoding='utf8') as f:
             continue
         if line == "":
             continue
+        if len(line.split(". ")) == 2:
+            line = line.split(". ")[1]
 
         line = line.replace("- ", "")
         bets[current_user].append(line)
 
-sorted_allsvenskan_tip_2025 = {k: v for k, v in sorted(allsvenskan_tip_2025.items(), key=lambda item: item[1])}
-table_data = [(team, value) for team, value in sorted_allsvenskan_tip_2025.items()]
+for user in bets.keys():
+    for i, bet in enumerate(bets[user]):
+        if bet not in list(allsvenskan_tip_2025.keys()):
+            allsvenskan_tip_2025[bet] = i
+        else:
+            allsvenskan_tip_2025[bet] += i
 
 print("# Grabbarnas Allsvenska 2025")
-print(tabulate(table_data, headers=["Team", "Value"], tablefmt="grid"))
 
-print("# Grabbarnas Bet")
+sorted_allsvenskan_tip_2025 = {k: v for k, v in sorted(allsvenskan_tip_2025.items(), key=lambda item: item[1])}
+table_data = [(team, value) for team, value in sorted_allsvenskan_tip_2025.items()]
+print(tabulate(table_data, headers=["Team", "Value"], tablefmt="grid") + "\n")
+
+print("# Grabbarns Bet")
 for user in bets.keys():
     print(f"## {user}")
     for i, bet in enumerate(bets[user]):
@@ -35,5 +44,3 @@ for user in bets.keys():
             allsvenskan_tip_2025[bet] += i
         print(f"{i + 1}. {bet}")
     print("")
-
-
